979a79eff90cd545376153a59aef2c5f953d7d56
Bart van Stratum <bart@vanstratum.com>
2015-01-12 14:34:52 +0100

    Added directory and scripts to run WPS from within OLGA structure

---
+++ WPS/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# Script to prepare directory for WPS routines
+
+# -------------------------
+# Change WPSPath to relative or absolute path to WPS installation directory
+WPSPath="../../WPS_v3.6.1"
+# -------------------------
+
+# Link the required files to current directory
+ln -s $WPSPath/*.exe .
+ln -s $WPSPath/link_grib.csh .
+ln -s $WPSPath/geogrid .
+ln -s $WPSPath/metgrid .
+ln -s $WPSPath/ungrib/Variable_Tables/Vtable.GFS Vtable
