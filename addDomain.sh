dfd61ce4ad6cc13fda41ec884f74a0c20beab325
julietbravo <bart@vanstratum.com>
2015-01-13 11:37:17 +0100

    Added bash script to add all required files for a new domain

---
+++ addDomain.sh
@@ -0,0 +1,16 @@
+# Add the required files for a new domain
+DOMAIN="OLGA_NL"
+
+git add $DOMAIN/domainSettings.py
+git add $DOMAIN/__init__.py
+git add $DOMAIN/matplotlibrc
+git add $DOMAIN/logs/README
+git add $DOMAIN/outputOLGA/README
+git add $DOMAIN/outputWRF/README
+git add $DOMAIN/WPS/prepare.sh
+git add $DOMAIN/WPS/namelist.wps
+git add $DOMAIN/WRF/prepare.sh
+git add $DOMAIN/WRF/namelist.input
+
+
+
