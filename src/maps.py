1c0c6ef8f93ab8e0844cc0d1b6d0aac355dd043b
julietbravo <bart@vanstratum.com>
2015-01-31 11:05:53 +0100

    Moved 'hard coded' figure settings to domainSettings.py, and update maps

--- src/maps.py
+++ src/maps.py
@@ -32,12 +32,12 @@ from tools import *
 """
 * Global settings: make property of settings?
 """
-fig_width_px  = 650  # width of figure [px]
-fig_dpi       = 100  # resolution in [px/in]
-mar_bottom_px = 20   # bottom margin [px]
-mar_top_px    = 25   # top margin in [px]
-mar_left_px   = 12   # left margin in [px]
-mar_right_px  = 80   # right margin in [px]
+#fig_width_px  = 650  # width of figure [px]
+#fig_dpi       = 100  # resolution in [px/in]
+#map_bottom_px = 20   # bottom margin [px]
+#map_top_px    = 25   # top margin in [px]
+#map_left_px   = 12   # left margin in [px]
+#map_right_px  = 80   # right margin in [px]
 
 """ 
 * Set the map properties, either from
@@ -75,30 +75,30 @@ def getDomain(olga, dom, wrf):
 * 1. Prescribed figure width in pixels
 * 2. Fixed margins for labels, colorbar, etc 
 """
-def getFigureProperties(map_height, map_width):
+def getFigureProperties(olga, map_height, map_width):
     # Aspect ratio of map
     map_aspect    = (float(map_height) / float(map_width))
    
     # Absolute size map
-    map_width_px  = fig_width_px - mar_left_px - mar_right_px # [px]
+    map_width_px  = olga.fig_width_px - olga.map_left_px - olga.map_right_px # [px]
     map_height_px = map_width_px * map_aspect # [px]
 
     # Relative left spacing and map width
-    ax_left       = float(mar_left_px)  / float(fig_width_px) 
-    ax_width      = float(map_width_px) / float(fig_width_px)
+    ax_left       = float(olga.map_left_px)  / float(olga.fig_width_px) 
+    ax_width      = float(map_width_px) / float(olga.fig_width_px)
    
     # Figure height
-    fig_height_px = map_height_px + mar_bottom_px + mar_top_px # [px]
+    fig_height_px = map_height_px + olga.map_bottom_px + olga.map_top_px # [px]
    
     # Relative bottom spacing and map height
-    ax_bot        = float(mar_bottom_px) / float(fig_height_px) # [-]
+    ax_bot        = float(olga.map_bottom_px) / float(fig_height_px) # [-]
     ax_height     = float(map_height_px) / float(fig_height_px) # [-]
     
     # Figure size in inches for matplotlib
-    fig_height_in = float(fig_height_px) / float(fig_dpi)
-    fig_width_in  = float(fig_width_px ) / float(fig_dpi)
+    fig_height_in = float(fig_height_px) / float(olga.fig_dpi)
+    fig_width_in  = float(olga.fig_width_px ) / float(olga.fig_dpi)
 
-    return (ax_left, ax_bot, ax_width, ax_height, fig_height_in, fig_width_in, fig_height_px, fig_width_px)
+    return (ax_left, ax_bot, ax_width, ax_height, fig_height_in, fig_width_in, fig_height_px)
 
 ## Function to create maps
 def create_maps(olga, wrfout, dom, times):
@@ -112,8 +112,8 @@ def create_maps(olga, wrfout, dom, times):
     map_lat, map_lon, map_width, map_height = getDomain(olga, dom, wrf)
 
     # Calculate figure properties
-    ax_left, ax_bot, ax_width, ax_height, fig_height_in, fig_width_in, fig_height_px, fig_width_px = \
-        getFigureProperties(map_height, map_width)
+    ax_left, ax_bot, ax_width, ax_height, fig_height_in, fig_width_in, fig_height_px = \
+        getFigureProperties(olga, map_height, map_width)
 
     # Setup basemap only once and (deep)copy later for performance
     basem = Basemap(width=map_width, height=map_height,
@@ -124,23 +124,18 @@ def create_maps(olga, wrfout, dom, times):
     # Read in the OLGA logo
     olga_logo = pl.matplotlib.image.imread(olga.olgaRoot+'include/olga_lr.png')
 
-    # Coordinates cities # TO-DO BVS: fix
-    if(False):
-        if(domain==1):
-            db = np.genfromtxt(olgaRoot+'include/cities_eur_d1.txt',dtype='str',delimiter=',',skip_header=0)
-        elif(domain==2):
-            db = np.genfromtxt(olgaRoot+'include/cities_eur_d2.txt',dtype='str',delimiter=',',skip_header=0)
-        city   = db[:,0]
-        citys  = db[:,1]  
-        citlon = np.array(db[:,2],dtype=np.float32)  
-        citlat = np.array(db[:,3],dtype=np.float32)  
-        citlon,citlat = basem(citlon,citlat)
+    # Coordinates cities
+    if(olga.drawCities[dom]):
+        cityLonM, cityLatM = basem(olga.cityLoc[dom].lon, olga.cityLoc[dom].lat)
+
+    # Default color line country boundaries
+    countryLines = '0.3'
 
     # Loop over differnt time steps 
     for t in times:
         print('Processing t=%i of %i'%(t,times[-1]))
         for var in olga.map_vars[dom]:
-            fig        = pl.figure(figsize=(fig_width_in, fig_height_in), dpi=fig_dpi)
+            fig        = pl.figure(figsize=(fig_width_in, fig_height_in), dpi=olga.fig_dpi)
             m          = deepcopy(basem)
             axloc      = [ax_left, ax_bot, ax_width, ax_height]
             ax         = fig.add_axes(axloc)
@@ -177,32 +172,34 @@ def create_maps(olga, wrfout, dom, times):
             # stream lines of wind
             # -------------------------------------------------
             if(var[:4] == 'wind'):
-                if(var=='wind10m'):
-                    title  = '10m wind (kts)'
-                    ufilt  = gausf(wrf.U10[t,:,:], fsigma,mode='reflect')
-                    vfilt  = gausf(wrf.V10[t,:,:], fsigma,mode='reflect')
-                    scalefac = 25.
-                elif(var=='wind1000m'):
-                    title  = '1000m wind'
-                    ufilt  = gausf(wrf.U1000[t,:,:], fsigma,mode='reflect')
-                    vfilt  = gausf(wrf.V1000[t,:,:], fsigma,mode='reflect')
-                    scalefac = 35.
-
-                utot   = ((ufilt[:,:]*m2k)**2. + (vfilt[:,:]*m2k)**2.)**0.5
-                umax   = 40. 
-                levs2  = np.arange(0, umax+0.01, umax/40.)  # Wind levels
-                cf     = m.contourf(lon, lat, utot, levs2, extend='both', cmap=wnd)
-                units  = 'kts'
-                stream = m.streamplot(lon[0,:], lat[:,0], ufilt[:,:], vfilt[:,:], density=3, linewidth=utot/scalefac, color='k')
                 doplot = True 
+                units  = 'kts'
+
+                if(var=='wind10m'):
+                    title = '10m wind (kts)'
+                    ufilt = gausf(wrf.U10[t,:,:], fsigma,mode='reflect')
+                    vfilt = gausf(wrf.V10[t,:,:], fsigma,mode='reflect')
+                    sf    = 25.
+                elif(var=='wind1000m'):
+                    title = '1000m wind'
+                    ufilt = gausf(wrf.U1000[t,:,:], fsigma,mode='reflect')
+                    vfilt = gausf(wrf.V1000[t,:,:], fsigma,mode='reflect')
+                    sf    = 35.
+
+                umax   = 40. 
+                levs   = np.arange(0, umax+0.01, 1.)
+                utot   = ((ufilt[:,:]*m2k)**2. + (vfilt[:,:]*m2k)**2.)**0.5
+                cf     = m.contourf(lon, lat, utot, levs, extend='both', cmap=wnd)
+                stream = m.streamplot(lon[0,:], lat[:,0], ufilt[:,:], vfilt[:,:], density=3, linewidth=utot/sf, color='k')
  
             # -------------------------------------------------
             # rain
             # -------------------------------------------------
-            if(var == 'rr'):
+            if(var == 'rain'):
                 doplot = True
                 title  = 'Precip. between t=%02i-%02i UTC (mm, //=convective)'%(wrf.hour[t-1],wrf.hour[t])
                 units  = 'mm'
+
                 if(t==0):
                     rr  = wrf.rr_mp[t,:,:] + wrf.rr_con[t,:,:]
                     rrc = wrf.rr_con[t,:,:]
@@ -213,6 +210,7 @@ def create_maps(olga, wrfout, dom, times):
                 # Draw shaded contours for precipitation intensity
                 levs   = [0,0.1,0.2,0.4,0.8,1,2,3,4,5,6,7,8,9,10,12,14]
                 cf     = m.contourf(lon, lat, rr, levs, alpha=1., extend='both', cmap=rain3nl)
+
                 # Hatch area where precipitation is convective
                 levs   = [0.1,100] 
                 cf2    = m.contourf(lon, lat, rrc, levs, colors='none', hatches=['//']) 
@@ -241,10 +239,24 @@ def create_maps(olga, wrfout, dom, times):
             # TO-DO: make it potential (i.e. max potential swd / actual swd)
             # -------------------------------------------------
             if(var == 'swd'):
-                title = 'Shortwave incoming radiation (W/m2)'
-                levs  = np.arange(0, 1000.01, 50)
-                data  = gausf(wrf.swd[t,:,:], fsigma,mode='reflect') if filter else wrf.swd[t,:,:]
-                cf    = m.contourf(lon, lat, data, levs, extend='both', cmap=wup)
+                title = 'Relative shortwave radiation (W/m2)'
+                levs  = np.arange(0, 1.01, 0.1)
+                data  = wrf.swd_frac[t,:,:] #gausf(wrf.swd_frac[t,:,:], fsigma,mode='reflect') if filter else 
+
+                cm_white = make_colormap({0.:'#ffffff',1.:'#ffffff'})
+                cm_white._init()
+                x = np.linspace(0,1,cm_white.N+3)
+                beta = 2
+                alpha = 1.-(np.exp(beta*x)-1)/(np.exp(beta)-1)
+                #alpha = np.linspace(1.0,.0,cm_white.N+3)
+                cm_white._lut[:,-1] = alpha
+
+                m.drawlsmask(land_color='#107b17', ocean_color='#10367b',resolution='i', grid=1.25)
+                #cf    = m.imshow(data, interpolation='nearest', cmap=cm_white, vmin=0, vmax=1)
+
+                levs = np.linspace(0,1,10)
+                cf    = m.contourf(lon, lat, data, levs, extend='both', cmap=cm_white, antialiased=True)
+
                 units = 'W/m2'
                 doplot = True 
  
@@ -264,7 +276,6 @@ def create_maps(olga, wrfout, dom, times):
                                        (119./256.,221./256.,255./256.),\
                                        (119./256.,255./256.,154./256.)], 'indexed')
 
-
                 if(t==0):
                     rr  = wrf.rr_mp[t,:,:] + wrf.rr_con[t,:,:]
                     rrc = wrf.rr_con[t,:,:]
@@ -280,7 +291,7 @@ def create_maps(olga, wrfout, dom, times):
                 bg = np.zeros((wrf.nlat, wrf.nlon))
                 for i in range(wrf.nlat):
                     for j in range(wrf.nlon):
-                        if(rain[i,j] > 0.1): # Rain
+                        if(rain[i,j] > 0.5): # Rain
                             bg[i,j] = 0.5
                         elif(wrf.swd_frac[t,i,j] < 0.1): 
                             bg[i,j] = 1.5
@@ -294,7 +305,6 @@ def create_maps(olga, wrfout, dom, times):
 
                 cf    = m.pcolormesh(lon, lat, bg, vmin=0, vmax=5, cmap=cmap)
                 units = '-'
-
  
                 intv = 3
                 for i in range(0, wrf.nlat, intv):
@@ -352,7 +362,7 @@ def create_maps(olga, wrfout, dom, times):
             # -------------------------------------------------
             if((var == 'pfd') and t < np.size(wrf.date_PFD)):
                 title = 'Potential flight distance [km]'
-                levs  = np.arange(0, 900.1, 100)
+                levs  = np.arange(0, 800.1, 100)
                 data  = gausf(wrf.PFD[t,:,:], fsigma,mode='reflect') if filter else wrf.PFD[t,:,:]
                 cf    =  m.contourf(lon, lat, data, levs, extend='both', cmap=wup)
                 units = 'km'
@@ -368,10 +378,10 @@ def create_maps(olga, wrfout, dom, times):
                     contour(lon, lat, wrf.hgt, levs, cmap=cm.PuBu, alpha=0.5)
 
                 # Plot cities
-                if(False):
-                    for i in range(len(citys)):
-                        m.scatter(citlon[i],citlat[i],s=4,alpha=1.0)
-                        pl.text(citlon[i],citlat[i],' '+citys[i],size=8,ha='left',va='center',color='0.1')
+                if(olga.drawCities[dom]):
+                    for i in range(len(cityLonM)):
+                        m.scatter(cityLonM[i], cityLatM[i], s=4, alpha=1.0)
+                        pl.text(cityLonM[i], cityLatM[i],' '+olga.cityLoc[dom].shortName[i], size=8, ha='left', va='center', color='0.1')
 
                 # Plot airspace
                 if(False):
@@ -388,12 +398,11 @@ def create_maps(olga, wrfout, dom, times):
                 pl.axes(ax)
           
                 # Draw coast and country outlines 
-                m.drawcoastlines(linewidth=1, color='0.3')
-                m.drawcountries(linewidth=1, color='0.3')
-                m.drawmapboundary()
+                m.drawcoastlines(linewidth=1.5, color=countryLines)
+                m.drawcountries(linewidth=1.5, color=countryLines)
             
                 # Draw rivers 
-                if(olga.drawRivers[dom] == True):
+                if(olga.drawRivers[dom]):
                     m.drawrivers(linewidth=0.5, color='#0066FF')
  
                 # Add description variables, units, OLGA label, logo, etc. 
@@ -409,7 +418,7 @@ def create_maps(olga, wrfout, dom, times):
                 ax.set_title(subtitle, loc='right', size=8)
 
                 # Add OLGA logo
-                pl.figimage(olga_logo, fig_width_px-45, 6)
+                pl.figimage(olga_logo, olga.fig_width_px-45, 6)
 
                 # Get name and save figure 
                 if(var=='pfd'):
@@ -420,10 +429,9 @@ def create_maps(olga, wrfout, dom, times):
                     minute = int((xtime - hour) * 60.)
                     tmp    = str(hour).zfill(4) + str(minute).zfill(2)
 
-                #name = '%s%04i%02i%02i_t%02iz/%s_d%i_%s.png'%(olga.figRoot,olga.year,olga.month,olga.day,olga.cycle,tmp,dom+1,var)
                 name = '%s%04i%02i%02i_t%02iz/%s_%02i_%s.png'%(olga.figRoot, olga.year, olga.month, olga.day, olga.cycle, var, dom+1, tmp)
 
-                pl.savefig(name, dpi=fig_dpi)
+                pl.savefig(name, dpi=olga.fig_dpi)
         
             # Cleanup!
             fig.clf()
