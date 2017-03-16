7e5bd4412d28d9f3d7b4151fb9315e09e458d41f
Roel Baardman <roel.baardman@gmail.com>
2017-03-16 15:56:37 +0100

    This solves issue #11 and issue #12

--- run.py
+++ run.py
@@ -1,24 +1,45 @@
 import datetime
 import math
+import importlib
 
 # Import OLGA specific routines
 from src.main import *
 
+#Usage: domain year month day cycle
+
+
 # Import file with domain settings from directory "domain_test" or other
 #from domain_test.domainSettings import olgaSettings as settings_d1
-from OLGA_NL.domainSettings import olgaSettings as settings_d1
+
+
 
 def myfloor(x, base=6):
 	return int(base * math.floor(float(x)/base))
 
-mode = 'all'
+if len(sys.argv) < 2:
+    print >>sys.stderr, "Usage: %s domain [mode] [year] [month] [day]" % sys.argv[0]
+    sys.exit(-1)
 
+
+domain = sys.argv[1]
+domainSettings = importlib.import_module("%s.domainSettings" % domain)
+settings_d1 = domainSettings.olgaSettings
 olga = settings_d1()
 
+mode = 'all'
 olga.year    = int(time.strftime('%Y'))
 olga.month   = int(time.strftime('%m'))
 olga.day     = int(time.strftime('%d'))
 #olga.cycle   = myfloor(int(time.strftime('%H')))
+
+if len(sys.argv) > 2:
+    mode = sys.argv[2]
+if len(sys.argv) > 3:
+    year = int(sys.argv[3])
+if len(sys.argv) > 4:
+    month = int(sys.argv[4])
+if len(sys.argv) > 5:
+    day = int(sys.argv[5])
 
 print('--------------------------------')
 print('Starting OLGA for %02i-%02i-%04i %02iz'%(olga.day, olga.month, olga.year, olga.cycle))
@@ -29,8 +50,6 @@ execute('rm -rf %s/*' % olga.figRoot)
 execute('rm -rf %s/*' % olga.wrfDataRoot)
 execute('rm -rf %s/*' % olga.gfsDataRoot)
 execute('rm -rf %s/*' % olga.olgaLogs)
-
-
 
 # Loop over the time slices
 startTime = datetime.datetime.now()
