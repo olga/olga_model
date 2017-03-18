c52f0a52a679bd0236e44e4999d74e8a97bfd720
Roel Baardman <roel.baardman@gmail.com>
2017-03-18 16:39:29 +0100

    Fix numerous namelist syntax errors
    or your changes. Lines starting

--- src/main.py
+++ src/main.py
@@ -154,13 +154,18 @@ def printn(string,n,separator=','):
         str_out += str(string) + str(separator)
     return str_out
 
+def printl(lst, separator=','):
+    ret = separator.join([str(x) for x in lst])
+    ret = ret + ","
+    return ret
+
 ## Update WRF and WPS namelists
 # @param olga Pointer to object with OLGA settings
 def updateNamelists(olga):
     printf('updating namelists WPS and WRF...')
 
-    dx = [a/b for a,b in zip(olga.map_width,olga.grid_we)]
-    dy = [a/b for a,b in zip(olga.map_height,olga.grid_sn)]
+    dx = [str(int(a/b)) for a,b in zip(olga.map_width,olga.grid_we)]
+    dy = [str(int(a/b)) for a,b in zip(olga.map_height,olga.grid_sn)]
 
     # Update WRF namelist
     namelist_input = olga.wrfRoot + 'namelist.input'
@@ -176,10 +181,10 @@ def updateNamelists(olga):
     replace(namelist_input,'end_hour',     printn(olga.endstruct.hour,     olga.ndom))
     replace(namelist_input,'end_minute',   printn(olga.endstruct.minute,   olga.ndom))
     replace(namelist_input,'end_second',   printn(olga.endstruct.second,   olga.ndom))
-    replace(namelist_input,'e_we',         printn(olga.grid_we,            olga.ndom))
-    replace(namelist_input,'e_sn',         printn(olga.grid_sn,            olga.ndom))
-    replace(namelist_input,'dx',           printn(dx,                      olga.ndom))
-    replace(namelist_input,'dy',           printn(dy,                      olga.ndom))
+    replace(namelist_input,'e_we',         printl(olga.grid_we))
+    replace(namelist_input,'e_sn',         printl(olga.grid_sn))
+    replace(namelist_input,' dx',           printl(dx))
+    replace(namelist_input,' dy',           printl(dy))
 
     # Set restart file frequency, restart flag and number of domains
     rflag = '.true.' if olga.islice>0 else '.false.'
@@ -203,10 +208,10 @@ def updateNamelists(olga):
     replace(namelist_wps,'end_second',    printn(olga.endstruct.second,   olga.ndom))
     replace(namelist_wps,'geog_data_path',str(olga.geogDataRoot.replace("/", "\/")))
     replace(namelist_wps,'max_dom',       str(olga.ndom))
-    replace(namelist_wps,'e_we',          printn(olga.grid_we,            3))
-    replace(namelist_wps,'e_sn',          printn(olga.grid_sn,            3))
-    replace(namelist_wps,'dx',            str(dx[0]))
-    replace(namelist_wps,'dy',            str(dy[0]))
+    replace(namelist_wps,'e_we',          printn(olga.grid_we[0],            3))
+    replace(namelist_wps,'e_sn',          printn(olga.grid_sn[0],            3))
+    replace(namelist_wps,' dx',            str(dx[0])+",")
+    replace(namelist_wps,' dy',            str(dy[0])+",")
 
 ## Run the WPS steps
 # @param olga Pointer to object with OLGA settings
